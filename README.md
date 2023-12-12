ff0089311012a0e207e72d4484888dda2c3d087a
Adin <andhika.bhayangkara@desaysv.com>
2023-12-12 13:48:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Learning Rust
+Just doing it for fun!
+
+## Source
+https://doc.rust-lang.org/book/
---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
+
+/*
+Constants:
+    const name : type = value;
+
+    - always immutable
+    - can be declared in any scope
+    - constant expression, not runtime const
+*/
